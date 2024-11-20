3892805180d0ce231a225847569e18650d2bcc68
elmudometal <elmudometal@users.noreply.github.com>
2024-11-20 18:35:48 +0000

    Fix styling

--- src/FilamentSelectTableServiceProvider.php
+++ src/FilamentSelectTableServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace ElmudoDev\FilamentSelectTable;
 
+use ElmudoDev\FilamentSelectTable\Commands\FilamentSelectTableCommand;
+use ElmudoDev\FilamentSelectTable\Testing\TestsFilamentSelectTable;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use ElmudoDev\FilamentSelectTable\Commands\FilamentSelectTableCommand;
-use ElmudoDev\FilamentSelectTable\Testing\TestsFilamentSelectTable;
 
 class FilamentSelectTableServiceProvider extends PackageServiceProvider
 {
